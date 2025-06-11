c5a9b642d7f59f5479f74cae38a976a5f4af00a5
OksMor <Sancho_2006_ru@mail.ru>
2025-06-11 11:40:42 +0300

    feat: add func

--- js/function.js
+++ js/function.js
@@ -38,3 +38,36 @@ stringToNumber('tttt1');
  */
 const extractNumbers = (string = '') => parseInt(string.replace(/\D+/g, ''), 10); // убрать все не числа
 extractNumbers('2ф-597');
+
+/**
+ * Функция перевода времени в минуты после полуночи
+ * @param {string} str - время в формате h:m
+ * @returns {number}
+ */
+const convertTime = (str) => {
+  const minutes = str.split(':').map(Number);
+  return minutes[0] * 60 + minutes[1];
+};
+
+/**
+ * Функция проверки входит ли время встречи в период рабочего времени
+ * @param {string} from - начало рабочего дня
+ * @param {string} to - конец рабочего дня
+ * @param {string} start - начало встречи
+ * @param {number} duration - продолжительность встречи в минутах
+ * @returns {boolean}
+ */
+const isDuring = (from , to, start, duration) => {
+  const fromDay = convertTime(from);
+  const toDay = convertTime(to);
+  const startMeeting = convertTime(start);
+  const durationMeeting = duration;
+
+  return fromDay <= startMeeting && (startMeeting + durationMeeting) <= toDay;
+};
+
+isDuring('08:00', '17:30', '14:00', 90); // true
+isDuring('8:0', '10:0', '8:0', 120); // true
+isDuring('08:00', '14:30', '14:00', 90); // false
+isDuring('14:00', '17:30', '08:0', 90); // false
+isDuring('8:00', '17:30', '08:00', 900); // false
